7b83e02b78f491af34d29a9961f3bfdf1212ec06
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2021-02-07 13:10:05 +1030

    move to issues

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -e
 
-# TODO Consider moving all the TODO to proper issues
 # TODO Consider allowing for non-2FA. Seems really insecure as the full credentials would linger in the container. Then again it's hardly secure anyway there's no access token expiry afaik.
 # TODO Better handling of existing but invalid access tokens. For now we blanket use existing as otherwise container restarts would attempt to auth using stale 2FA token and fail. The downside of this is when we have an expired access token we need to manually remove it to trigger retrieval of a new access token.
 
